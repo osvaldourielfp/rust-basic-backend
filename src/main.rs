2c1959e32e190b612d6ec3a584fb0049b2f9f491
Osvaldo Frias <osvaldourielfp@gmail.com>
2021-01-08 22:38:28 -0600

    Conexión HTTP

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
+use std::fs;
+use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7373").unwrap();
-    
-    for stream in listener.incoming() {
-    	match stream {
-        	Ok(stream) => {
-            	println!("¡Nuevo cliente!");
-        	}
-            	Err(e) => {
-                	println!("Conexión fallida")
-            	}
-    	}
-	}
 
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 256];
+    stream.read(&mut buffer).unwrap();
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let contents = fs::read_to_string("index.html").unwrap();
+    // let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
+    stream.write(response.as_bytes()).unwrap(); //Nos ayuda a leer la cadena bytes que estamos recibiendo.
+    stream.flush().unwrap(); //Esperará e impedirá que el programa continúe hasta que se escriban todos los bytes en la conexión.
 }
