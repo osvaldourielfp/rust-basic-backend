a1a3e42686aba835270fc625daa4400cac57665a
Osvaldo Frias <osvaldourielfp@gmail.com>
2021-01-08 22:22:16 -0600

    Conexión TCP

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7373").unwrap();
+    
+    for stream in listener.incoming() {
+    	match stream {
+        	Ok(stream) => {
+            	println!("¡Nuevo cliente!");
+        	}
+            	Err(e) => {
+                	println!("Conexión fallida")
+            	}
+    	}
+	}
+
+}
