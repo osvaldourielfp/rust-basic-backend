b5129f1517bf9793ccfb7fd0c1e4eff301004f7b
Osvaldo Frias <osvaldourielfp@gmail.com>
2021-01-08 23:28:48 -0600

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -18,28 +18,30 @@ fn handle_connection(mut stream: TcpStream) {
     let get = b"GET / HTTP/1.1\r\n";
 
     if buffer.starts_with(get) {
-
+        let status_line = "HTTP/1.1 200 OK";
         let contents = fs::read_to_string("index.html").unwrap();
-        
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
-        stream.write(response.as_bytes()).unwrap(); //Nos ayuda a leer la cadena bytes que estamos recibiendo.
-        stream.flush().unwrap(); //Esperará e impedirá que el programa continúe hasta que se escriban todos los bytes en la conexión.
+
+        let response = format_response(status_line.to_string(), contents.to_string());
+        send_response(stream, response);
     } else {
         let status_line = "HTTP/1.1 404 NOT FOUND";
         let contents = fs::read_to_string("404.html").unwrap();
 
-        let response = format!(
-            "{}\r\nContent-Length: {}\r\n\r\n{}",
-            status_line,
-            contents.len(),
-            contents
-        );
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+        let response = format_response(status_line.to_string(), contents.to_string());
+        send_response(stream, response);
     }
+}
+
+fn format_response(status_line: String, contents: String) -> String {
+    return format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+}
+
+fn send_response(mut stream: TcpStream, response: String) {
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
